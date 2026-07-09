a80ba128b95538fecc90e0b77f29a6975f385e4c
luengof67 <luengof67@gmail.com>
2026-07-09 02:10:39 +0200

    Update informe_compra_service.dart

--- lib/services/informe_compra_service.dart
+++ lib/services/informe_compra_service.dart
@@ -3,6 +3,7 @@ import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
 import '../models/comparativa.dart';
+import '../models/producto.dart';
 import '../ui/formato.dart';
 
 /// Una línea de la compra óptima: un producto asignado a su proveedor más barato.
